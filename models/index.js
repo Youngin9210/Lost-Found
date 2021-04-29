432e6e45667d007ab68ddff80beffbc7ba46d3eb
Kyle <kyleyoung.9210@gmail.com>
2021-04-29 19:08:43 -0400

    finished model associations

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Item = require('./Item');
 
+User.hasMany(Item, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+})
+
+Item.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+
 module.exports = { User, Item };
