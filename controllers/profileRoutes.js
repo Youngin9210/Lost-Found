bd065d65577b21ae65257b9368981cf5bcccfab0
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-05-01 13:40:11 -0400

    user items are now displayed in user profile, can be deleted

---
+++ controllers/profileRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { User, Item } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    console.log('/');
+    try {
+      // Get all items and JOIN with user data
+      const itemData = await Item.findAll({
+        where: {user_id: req.session.user_id},
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      });
+  
+      // // Serialize data so the template can read it
+      const items = itemData.map((item) => item.get({ plain: true }));
+      // Pass serialized data and session flag into template
+      res.render('profile', { items, logged_in: req.session.logged_in });
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  });
+
+  module.exports = router;
