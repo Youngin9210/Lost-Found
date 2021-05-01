1a6f1f659e7573cbbbb6d9acf6b64784e185ef3a
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-05-01 13:06:01 -0400

    updated api routes to show item info, added else statement to profile.handlebars for testing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,27 +24,27 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.get('/item/:id', async (req, res) => {
-//   try {
-//     const itemData = await Item.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
+router.get('/item/:id', async (req, res) => {
+  try {
+    const itemData = await Item.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
 
-//     const item = itemData.get({ plain: true });
+    const item = itemData.get({ plain: true });
 
-//     res.render('item', {
-//       ...item,
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('item', {
+      ...item,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
