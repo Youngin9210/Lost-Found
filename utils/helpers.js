8dbdf1a24c250d432c36c82ff389204b5f9fa859
Kyle <kyleyoung.9210@gmail.com>
2021-05-05 13:52:10 -0400

    added a method to capitalize the first letter of the username

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,4 +3,8 @@ module.exports = {
     // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
   },
+
+  capitalize: (username) => {
+    return username[0].toUpperCase() + username.slice(1);
+  },
 };
