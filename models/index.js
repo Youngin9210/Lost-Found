c08f3b4a56a2367d130993197a9b73f17b14e3f5
Kyle <kyleyoung.9210@gmail.com>
2021-05-06 23:59:36 -0400

    added notification table and associations

--- models/index.js
+++ models/index.js
@@ -1,13 +1,31 @@
 const User = require('./User');
 const Item = require('./Item');
+const Notification = require('./Notification');
 
 User.hasMany(Item, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-})
+  onDelete: 'CASCADE',
+});
+
+User.hasMany(Notification, {
+  foreignKey: 'item_owner',
+  onDelete: 'CASCADE',
+});
+
+Item.hasOne(Notification, {
+  foreignKey: 'item_id',
+});
 
 Item.belongsTo(User, {
-  foreignKey: 'user_id'
-})
+  foreignKey: 'user_id',
+});
 
-module.exports = { User, Item };
+Notification.belongsTo(User, {
+  foreignKey: 'item_owner',
+});
+
+Notification.belongsTo(Item, {
+  foreignKey: 'item_id',
+});
+
+module.exports = { User, Item, Notification };
