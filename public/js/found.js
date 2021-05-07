51a3f015c1c7345058d8210eb5351f380a179c18
Kyle <kyleyoung.9210@gmail.com>
2021-05-07 00:07:50 -0400

    added logic to handle posting a new notification

--- public/js/found.js
+++ public/js/found.js
@@ -4,18 +4,25 @@ const foundItem = async (event) => {
   event.preventDefault();
 
   const isFound = foundBtn.getAttribute('data-isFound');
-
   const id = foundBtn.getAttribute('data-item');
-  const user_id = foundBtn.getAttribute('data-user');
+  const found_user = foundBtn.getAttribute('data-current');
+  const item_owner = foundBtn.getAttribute('data-user');
   const logged_in = foundBtn.getAttribute('data-isLoggedIn');
 
   if (logged_in) {
-    const response = await fetch(`/api/items/isFound/${id}`, {
+    const responseUpdate = await fetch(`/api/items/isFound/${id}`, {
       method: 'PUT',
-      body: JSON.stringify({ isFound, id }),
+      body: JSON.stringify({ isFound }),
       headers: { 'Content-Type': 'application/json' },
     });
-    response.ok
+
+    const responsePost = await fetch(`/api/notifications`, {
+      method: 'POST',
+      body: JSON.stringify({ item_owner, id, found_user }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    responseUpdate.ok && responsePost.ok
       ? document.location.replace('/')
       : alert('Failed to update item');
   } else {
